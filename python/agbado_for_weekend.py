7f3eb5ee3a6c5c1b64d6820aa08125c7d0deacf7
michael philip <philipmichaelonyekachi>
2025-10-13 13:45:24 +0100

    update

---
+++ python/agbado_for_weekend.py
@@ -0,0 +1,46 @@
+def get_first_and_last_two
+	if len(word)<2:
+	   return " " 
+	first_two = word[0]  + word[1]
+	last_two = word[-2]  + word[-1]
+	   return first_two + last_two
+
+
+def find_maximum[numbers]:
+	maximum = numbers[0]
+
+    	for i in range(len(number)):
+
+		if numbers[i] > maximum:
+	  		 maximum = number[i]
+    	return maximum
+
+
+def find_minimum[numbers]:
+	minimum = numbers[0]
+
+   	for i in range(len(number)):
+
+		if numbers[i] < minimum:
+		 	  minimum = number[i]
+  	return maximum
+
+
+def get_longest_word[word]:
+    	longest = word{0]
+
+	for i in range(len(words)):
+
+		if len(word[i] > len(longest):
+			longest = word[i]
+	return longest
+			
+
+
+
+
+
+
+
+
+ 
