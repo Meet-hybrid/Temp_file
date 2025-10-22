796387247a1365d4a9779fde0b4b7943c6cf51b5
michael philip <philipmichaelonyekachi>
2025-10-22 07:44:48 +0100

    updating my delivery assignment

--- delivery_service/main.py
+++ delivery_service/main.py
@@ -5,8 +5,7 @@ main= """
     |_______________________________________________|  
     |Collection Rate   |Amount Per Percel|Base Pay  |
     |__________________|_________________|__________|
-    | 1 |Less than 50% |           160   | 5
-000     |
+    | 1 |Less than 50% |           160   | 5000     |
     |__________________|_________________|__________|
     | 2 |50% - 59%     |           200   | 5000     |
     |__________________|_________________|__________|
@@ -21,25 +20,26 @@ print(main)
 while True:
 	choice = int(input("\nSelect case (1-5): "))
 
-match choice:
-	case 1:
-		percent = float(input("Enter percentage (<50): "))
-		print(f"Rider's wage: {get_riders_wage(percent)}")			
-	case 2:					
-		percent = float(input("Enter percentage (50–59): "))
-		print(f"Rider's wage: {get_riders_wage(percent)}")
-	case 3:
-		percent = float(input("Enter percentage (60–69): "))
-		print(f"Rider's wage: {get_riders_wage(percent)}")
-	case 4:		
-		percent = float(input("Enter percentage (70–100): "))
-		print(f"Rider's wage: {get_riders_wage(percent)}")
-	case 5:
-		print("Exiting program... Goodbye!")
-		break
-	case _:
-		print("Invalid choice, try again.")
+	match choice:
+		case 1:
+			percent = float(input("Enter percentage (<50): "))
+			print(f"Rider's wage: {get_riders_wage(percent)}")			
+		case 2:					
+			percent = float(input("Enter percentage (50–59): "))
+			print(f"Rider's wage: {get_riders_wage(percent)}")
+		case 3:
+			percent = float(input("Enter percentage (60–69): "))
+			print(f"Rider's wage: {get_riders_wage(percent)}")
+		case 4:		
+			percent = float(input("Enter percentage (70–100): "))
+			print(f"Rider's wage: {get_riders_wage(percent)}")
 		
+		case 5:
+			print("Exiting program... Goodbye!")
+			break
+		case _:
+			print("Invalid choice, try again.")
+			
         
         
             
