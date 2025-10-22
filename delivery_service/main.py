e32460ad816cdd780a748c5c942f7a05187e6758
michael philip <philipmichaelonyekachi>
2025-10-22 07:38:52 +0100

    updating my delivery assignment

--- delivery_service/main.py
+++ delivery_service/main.py
@@ -5,7 +5,8 @@ main= """
     |_______________________________________________|  
     |Collection Rate   |Amount Per Percel|Base Pay  |
     |__________________|_________________|__________|
-    | 1 |Less than 50% |           160   | 5000     |
+    | 1 |Less than 50% |           160   | 5
+000     |
     |__________________|_________________|__________|
     | 2 |50% - 59%     |           200   | 5000     |
     |__________________|_________________|__________|
@@ -18,33 +19,28 @@ main= """
 """
 print(main)
 while True:
-        try:
-            choice = int(input("\nSelect case (1-5): "))
+	choice = int(input("\nSelect case (1-5): "))
 
-            match choice:
-                case 1:
-                    percent = float(input("Enter percentage (<50): "))
-                    print(f"Rider's wage: {get_riders_wage(percent)}")
-
-                case 2:
-                    percent = float(input("Enter percentage (50–59): "))
-                    print(f"Rider's wage: {get_riders_wage(percent)}")
-
-                case 3:
-                    percent = float(input("Enter percentage (60–69): "))
-                    print(f"Rider's wage: {get_riders_wage(percent)}")
-
-                case 4:
-                    percent = float(input("Enter percentage (70–100): "))
-                    print(f"Rider's wage: {get_riders_wage(percent)}")
-
-                case 5:
-                    print("Exiting program... Goodbye!")
-                    break
-
-                case _:
-                    print("Invalid choice, try again.")
+match choice:
+	case 1:
+		percent = float(input("Enter percentage (<50): "))
+		print(f"Rider's wage: {get_riders_wage(percent)}")			
+	case 2:					
+		percent = float(input("Enter percentage (50–59): "))
+		print(f"Rider's wage: {get_riders_wage(percent)}")
+	case 3:
+		percent = float(input("Enter percentage (60–69): "))
+		print(f"Rider's wage: {get_riders_wage(percent)}")
+	case 4:		
+		percent = float(input("Enter percentage (70–100): "))
+		print(f"Rider's wage: {get_riders_wage(percent)}")
+	case 5:
+		print("Exiting program... Goodbye!")
+		break
+	case _:
+		print("Invalid choice, try again.")
+		
         
-        except ValueError:
-            print("Please enter a valid number.")
+        
+            
 
